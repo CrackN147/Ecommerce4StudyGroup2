b24bc1327adc94933a59134c489a6f3383f2b5b5
CR <george.artenyan@gmail.com>
2023-04-10 21:25:01 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './css/App.css';
+import {Header, Footer} from './pages';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer pageName='Hello' />
+    </>
+  )
+}
+
+export default App;
---
+++ src/pages/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer (props) {
+  const {pageName} = props;
+  return (
+    <footer>
+      <p>© 2021 - My Company</p>
+    </footer>
+  )
+}
